7c3058e77413a0d07bee561d3c72f442d091e94e
Herney Ruiz <herneyruiz36@gmail.com>
2020-07-25 15:25:25 -0500

    view home login

--- resources/views/pages/home/home.blade.php
+++ resources/views/pages/home/home.blade.php
@@ -1,14 +1,56 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>{{ env('APP_NAME', 'Escuentas | Covid') }}</title>
     <link rel="stylesheet" href="{{ asset('css/stylus.css') }}">
 </head>
-<body>
-    <h1>Login</h1>
+
+<body class="login-page" style="min-height: 512.391px;">
+
+    <div class="login-box">
+        <div class="login-logo">
+            <a href="#"><b>Encuestas</b>Covid</a>
+        </div>
+        <!-- /.login-logo -->
+        <div class="card">
+            <div class="card-body login-card-body">
+                <p class="login-box-msg">Inicia sesión para comenzar tu sesión</p>
+
+                <form action="#" method="post">
+                    <div class="input-group mb-3">
+                        <input type="user" class="form-control" placeholder="Usuario">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-envelope"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="password" class="form-control" placeholder="Contraseña">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <!-- /.col -->
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-primary w-100">Inicias Sesión</button>
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                </form>
+            </div>
+            <!-- /.login-card-body -->
+        </div>
+    </div>
+    <!-- /.login-box -->
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
+
 </html>
