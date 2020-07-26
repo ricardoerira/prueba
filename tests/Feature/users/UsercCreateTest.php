3bacbcbe1ba75aae366a5970dc87a173404e476b
Herney Ruiz <herneyruiz36@gmail.com>
2020-07-26 10:58:11 -0500

    user create test

--- tests/Feature/users/UsercCreateTest.php
+++ tests/Feature/users/UsercCreateTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\users;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -62,6 +63,35 @@ class UserCreateTest extends TestCase
         $response = $this->get(route('users.create'));
 
         $response->assertStatus(200);
+    }
+
+    /**
+     * @return void
+     * @test
+     */
+    public function user_authenticated_can_create_users(): void
+    {
+        $this->withoutExceptionHandling();
+
+        $user = factory(User::class)->create();
+        $role = factory(Role::class)->create();
+
+        $this->actingAs($user);
+
+        $userPost = [
+            "name"              => "juan camilo",
+            "email"             => "juancamilo@gmail.com",
+            "password"          => "password123",
+            'role_id'           => $role->id,
+        ];
+
+        $response = $this->from(route('users.create'))->post(route('users.save'), $userPost);
+
+        $response->assertStatus(302);
+
+        $response->assertRedirect(route('users.index'));
+
+        $this->assertDatabaseHas('users', $userPost);
     }
 
 }
