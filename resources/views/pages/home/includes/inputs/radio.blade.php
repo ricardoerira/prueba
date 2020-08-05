7519cd4ab0ad4dec5f02af5da544b5fb82cd0c5d
Herney Ruiz <herneyruiz36@gmail.com>
2020-08-05 11:53:06 -0500

    fieldset in radio inputs

--- resources/views/pages/home/includes/inputs/radio.blade.php
+++ resources/views/pages/home/includes/inputs/radio.blade.php
@@ -1,15 +1,17 @@
 <div class="form-check">
-    @foreach ($question->choices as $choice)
-        <input
-            type="radio"
-            value="{{ $choice->id }}"
-            name="answers[]"
-            class="form-check-input"
-            @if ( $question->answer_required_yn  == 1)
-                required
-            @endif
-        >
-        <label for="answers[]" class="form-check-label">{{ $choice->name }}</label>
-        <br>
-    @endforeach
+    <fieldset>
+        @foreach ($question->choices as $choice)
+            <input
+                type="radio"
+                value="{{ $choice->id }}"
+                name="answers[]"
+                class="form-check-input"
+                @if ( $question->answer_required_yn  == 1)
+                    required
+                @endif
+            >
+            <label for="answers[]" class="form-check-label">{{ $choice->name }}</label>
+            <br>
+        @endforeach
+    <fieldset>
 </div>
