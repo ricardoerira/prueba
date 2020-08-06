56c0cb4b5165896c6b2687a835d34c78aac9190e
Ricardo erira <wilsonricardo@wilsons-MacBook-Pro.local>
2020-08-05 19:18:50 -0500

    validate first form

--- app/Models/Header.php
+++ app/Models/Header.php
@@ -75,9 +75,15 @@ class Header extends Model
      */
     public function surveys()
     {
-        return $this->hasMany(Survey::class)
-            ->where('surveyed_id', Auth::id())
-            ->whereDate('surveys.created_at', Carbon::now()->format('Y-m-d'));
+        if ($this->id == 3) {
+            return $this->hasMany(Survey::class)
+                ->where('surveyed_id', Auth::id())
+                ->whereDate('surveys.completion_time', Carbon::now()->format('Y-m-d'));
+        } else {
+            return $this->hasMany(Survey::class)
+                ->where('surveyed_id', Auth::id());
+        }
+
     }
 
 }
