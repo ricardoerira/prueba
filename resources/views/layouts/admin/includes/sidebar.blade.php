6b46702aa3599a882da8e2e58840b8f47c1339b8
Ricardo Erira <ricardoerira@gmail.com>
2020-08-10 14:09:07 -0500

    logo

--- resources/views/layouts/admin/includes/sidebar.blade.php
+++ resources/views/layouts/admin/includes/sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
-        <img src="{{ asset('images/admin/logo/logo.png') }}" alt="Logotipo de AdminLTE"
+        <img src="{{ asset('images/admin/logo/logo.png') }}" alt="Logotipo de idpac"
             class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">
             <font style="vertical-align: inherit;">
