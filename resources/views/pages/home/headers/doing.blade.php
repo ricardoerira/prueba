f68012fa67e9b81c34e4635ca1b6324a4d7083e5
Ricardo Erira <ricardoerira@gmail.com>
2020-08-12 15:22:22 -0500

    survey correction 6 conditional questions

--- resources/views/pages/home/headers/doing.blade.php
+++ resources/views/pages/home/headers/doing.blade.php
@@ -57,7 +57,7 @@
         let header = $("#header_id").val();
 
         if (header == 2) {
-            $('.seccion-18').addClass("d-none")
+            $('.seccion-188').addClass("d-none")
 
             $('#radio_depended_0').change(() => {
                 $('.seccion-18').removeClass("d-none")
@@ -107,18 +107,19 @@
             $('.seccion-10').addClass("d-none")
             $('.seccion-11').addClass("d-none")
             $('.seccion-12').addClass("d-none")
-            $('.seccion-13').addClass("d-none")
+            $('.seccion-27').addClass("d-none")
             $('.seccion-14').addClass("d-none")
-            $('.seccion-15').addClass("d-none")
-            $('.seccion-16').addClass("d-none")
+            $('.seccion-28').addClass("d-none")
+            $('.seccion-29').addClass("d-none")
             $('.seccion-17').addClass("d-none")
             $('.seccion-18').addClass("d-none")
-            $('.seccion-19').addClass("d-none")
             $('.seccion-20').addClass("d-none")
             $('.seccion-21').addClass("d-none")
             $('.seccion-22').addClass("d-none")
             $('.seccion-23').addClass("d-none")
             $('.seccion-24').addClass("d-none")
+            $('.seccion-25').addClass("d-none")
+            $('.seccion-26').addClass("d-none")
 
             $('#radio_depended_125_0').change(() => {
                 $('.seccion-4').removeClass("d-none")
@@ -129,17 +130,307 @@
 
             $('#radio_depended_125_1').change(() => {
                 $('.seccion-4').addClass("d-none")
-                $(".depend").each(function() {
-                    $(this).addClass("d-none");
-                }).get();
+                $('.seccion-5').addClass("d-none")
+                $('.seccion-6').addClass("d-none")
+                $('.seccion-7').addClass("d-none")
+                $('.seccion-8').addClass("d-none")
+                $('.seccion-9').addClass("d-none")
+                $('.seccion-10').addClass("d-none")
+                $('.seccion-11').addClass("d-none")
+                $('.seccion-12').addClass("d-none")
+                $('.seccion-27').addClass("d-none")
+                $('.seccion-14').addClass("d-none")
+                $('.seccion-28').addClass("d-none")
+                $('.seccion-29').addClass("d-none")
+                $('.seccion-17').addClass("d-none")
+                $('.seccion-18').addClass("d-none")
+                $('.seccion-20').addClass("d-none")
+                $('.seccion-21').addClass("d-none")
+                $('.seccion-22').addClass("d-none")
+                $('.seccion-23').addClass("d-none")
+                $('.seccion-24').addClass("d-none")
+                $('.seccion-25').addClass("d-none")
+                $('.seccion-26').addClass("d-none")
+                
+            });
+            $('#radio_depended_126_0').change(() => {
+                $('.seccion-6').removeClass("d-none")
+                $('.seccion-5').addClass("d-none")
+                $('.seccion-17').addClass("d-none")
+                $('.seccion-23').addClass("d-none")
+                $('.seccion-24').addClass("d-none")
+
+                $('#radio_depended_128_0').change(() => {
+                    $('.seccion-7').removeClass("d-none")
+                    $('.seccion-11').addClass("d-none")
+                    $('.seccion-28').addClass("d-none")
+                    $('.seccion-29').addClass("d-none")
+                    $('.seccion-22').addClass("d-none")
+                    $('.seccion-27').addClass("d-none")
+                    $('#radio_depended_130_0').change(() => {   
+                        $('.seccion-8').removeClass("d-none")
+                        $('#radio_depended_131_0').change(() => {
+                            $('.seccion-10').addClass("d-none")
+                            $('.seccion-28').addClass("d-none")
+                            $('.seccion-29').addClass("d-none")
+                            $('.seccion-9').removeClass("d-none")
+                            $('#radio_depended_132_0').change(() => {
+                                $('.seccion-28').removeClass("d-none")
+                                $('#radio_depended_137_0').change(() => {
+                                    $('.seccion-29').removeClass("d-none")
+                                });
+                                $('#radio_depended_137_1').change(() => {
+                                    $('.seccion-29').addClass("d-none")
+                                });
+                            });
+                        });
+                        $('#radio_depended_131_1').change(() => {
+                            $('.seccion-9').addClass("d-none")
+                            $('.seccion-10').addClass("d-none")
+                            $('.seccion-28').addClass("d-none")
+                            $('.seccion-29').addClass("d-none")
+                            $('.seccion-10').removeClass("d-none")
+                            $('#radio_depended_133_0').change(() => {
+                                $('.seccion-28').removeClass("d-none")
+                                $('#radio_depended_137_0').change(() => {
+                                    $('.seccion-29').removeClass("d-none")
+                                });
+                                $('#radio_depended_137_1').change(() => {
+                                    $('.seccion-29').addClass("d-none")
+                                });
+                            });
+
+
+                        });
+                    });
+                    
+                });
+
+                $('#radio_depended_128_1').change(() => {
+                    $('.seccion-11').removeClass("d-none")
+                    $('.seccion-7').addClass("d-none")
+                    $('.seccion-8').addClass("d-none")
+                    $('.seccion-9').addClass("d-none")
+                    $('.seccion-10').addClass("d-none")
+                    $('.seccion-28').addClass("d-none")
+                    $('.seccion-29').addClass("d-none")
+                    $('#radio_depended_134_0').change(() => {
+                        
+                        $('.seccion-22').removeClass("d-none")
+                        $('#radio_depended_135_0').change(() => {
+                            $('.seccion-29').removeClass("d-none")
+                            $('#radio_depended_135_0').change(() => {
+                                $('.seccion-27').removeClass("d-none")
+                                $('.seccion-14').addClass("d-none")
+                                $('.seccion-28').addClass("d-none")
+                                $('.seccion-29').addClass("d-none")
+                                $('#radio_depended_150_0').change(() => {
+                                    $('.seccion-28').removeClass("d-none")
+                                    $('#radio_depended_137_0').change(() => {
+                                        $('.seccion-29').removeClass("d-none")
+                                    });
+                                });
+                            });
+                            $('#radio_depended_135_1').change(() => {
+                                $('.seccion-28').removeClass("d-none")
+                                $('.seccion-27').addClass("d-none")
+                                $('#radio_depended_137_0').change(() => {
+                                    $('.seccion-29').removeClass("d-none")
+                                });
+                                $('#radio_depended_137_1').change(() => {
+                                    $('.seccion-29').addClass("d-none")
+                                });
+                            });
+                       
+                        });
+                    });
+                });
             });
 
-            $('#radio_depended_126_0').change(() => {
+            $('#radio_depended_126_1').change(() => {
                 $('.seccion-5').removeClass("d-none")
-                $(".depend").each(function() {
-                    $(this).removeClass("d-none");
-                }).get();
+                $('.seccion-6').addClass("d-none")
+                $('.seccion-11').addClass("d-none")
+                $('.seccion-7').addClass("d-none")
+                $('.seccion-8').addClass("d-none")
+                $('.seccion-9').addClass("d-none")
+                $('.seccion-10').addClass("d-none")
+                $('.seccion-20').addClass("d-none")
+                $('.seccion-22').addClass("d-none")
+                $('.seccion-27').addClass("d-none")
+                $('.seccion-28').addClass("d-none")
+                $('.seccion-29').addClass("d-none")
+                $('#radio_depended_127_0').change(() => {
+                    $('.seccion-17').removeClass("d-none")
+                    $('.seccion-28').addClass("d-none")
+                    $('.seccion-29').addClass("d-none")
+                    $('.seccion-8').addClass("d-none")
+                    $('#radio_depended_139_0').change(() => {
+                        $('.seccion-23').addClass("d-none")
+                        $('.seccion-22').addClass("d-none")
+                        $('.seccion-26').addClass("d-none")
+                        $('.seccion-24').addClass("d-none")
+                        $('.seccion-28').addClass("d-none")
+                        $('.seccion-29').addClass("d-none")
+                        $('.seccion-18').removeClass("d-none")
+                        $('#radio_depended_142_0').change(() => {
+                            $('.seccion-20').removeClass("d-none")
+                            $('.seccion-21').addClass("d-none")
+                            $('.seccion-22').addClass("d-none")
+                            $('.seccion-27').addClass("d-none")
+                            $('.seccion-28').addClass("d-none")
+                            $('.seccion-29').addClass("d-none")
+                            $('#radio_depended_151_0').change(() => {
+                                $('.seccion-28').removeClass("d-none")
+                                $('#radio_depended_137_0').change(() => {
+                                    $('.seccion-29').removeClass("d-none")
+                                });
+                                $('#radio_depended_137_1').change(() => {
+                                    $('.seccion-29').addClass("d-none")
+                                });
+
+                            });
+                        });
+                        $('#radio_depended_142_1').change(() => {
+                            $('.seccion-20').addClass("d-none")
+                            $('.seccion-27').addClass("d-none")
+                            $('.seccion-28').addClass("d-none")
+                            $('.seccion-29').addClass("d-none")
+                            $('.seccion-21').removeClass("d-none")
+                            $('#radio_depended_152_0').change(() => {
+                                $('.seccion-29').addClass("d-none")
+                                $('.seccion-28').addClass("d-none")
+                                $('.seccion-22').removeClass("d-none")
+                                $('#radio_depended_135_0').change(() => {
+                                    $('.seccion-29').removeClass("d-none")
+                                    $('#radio_depended_135_0').change(() => {
+                                        $('.seccion-27').removeClass("d-none")
+                                        $('.seccion-14').addClass("d-none")
+                                        $('.seccion-28').addClass("d-none")
+                                        $('.seccion-29').addClass("d-none")
+                                        $('#radio_depended_150_0').change(() => {
+                                            $('.seccion-28').removeClass("d-none")
+                                            $('#radio_depended_137_0').change(() => {
+                                                $('.seccion-29').removeClass("d-none")
+                                    
+                                            });
+                                        });
+                                    });
+                                    $('#radio_depended_135_1').change(() => {
+                                        $('.seccion-28').removeClass("d-none")
+                                        $('.seccion-27').addClass("d-none")
+                                        $('#radio_depended_137_0').change(() => {
+                                            $('.seccion-29').removeClass("d-none")
+                                        });
+                                        $('#radio_depended_137_1').change(() => {
+                                            $('.seccion-29').addClass("d-none")
+                                        });
+                                    });
+                       
+                                });
+                                $('#radio_depended_135_1').change(() => {
+                                    $('.seccion-29').addClass("d-none")
+                                });
+                            });
+                           
+
+                        });
+                    });
+                    $('#radio_depended_139_1').change(() => {
+                        $('.seccion-21').addClass("d-none")
+                        $('.seccion-22').addClass("d-none")
+                        $('.seccion-27').addClass("d-none")
+                        $('.seccion-28').addClass("d-none")
+                        $('.seccion-29').addClass("d-none")
+                        $('.seccion-18').addClass("d-none")
+                        $('.seccion-26').addClass("d-none")
+                        $('.seccion-23').removeClass("d-none")
+                        $('#radio_depended_143_0').change(() => {
+                            $('.seccion-28').addClass("d-none")
+                            $('.seccion-29').addClass("d-none")
+                            $('.seccion-25').addClass("d-none")
+                            $('.seccion-24').removeClass("d-none")
+                            $('#radio_depended_145_0').change(() => {
+                                $('.seccion-28').removeClass("d-none")
+                                $('#radio_depended_137_0').change(() => {
+                                    $('.seccion-29').removeClass("d-none")
+                                });
+                                $('#radio_depended_137_1').change(() => {
+                                    $('.seccion-29').addClass("d-none")
+                                });
+                            });
+                            $('#radio_depended_145_1').change(() => {
+                                $('.seccion-28').removeClass("d-none")
+                                
+                            });
+                        });
+                        $('#radio_depended_143_1').change(() => {
+                            $('.seccion-24').addClass("d-none")
+                            $('.seccion-25').removeClass("d-none")
+                            $('#radio_depended_153_0').change(() => {
+                                $('.seccion-28').removeClass("d-none")
+                                $('#radio_depended_137_0').change(() => {
+                                    $('.seccion-29').removeClass("d-none")
+                                });
+                                $('#radio_depended_137_1').change(() => {
+                                    $('.seccion-29').addClass("d-none")
+                                });
+                            });
+                            
+                        });
+                    });
+                    $('#radio_depended_139_2').change(() => {
+                        $('.seccion-18').addClass("d-none")
+                        $('.seccion-23').addClass("d-none")
+                        $('.seccion-24').addClass("d-none")
+                        $('.seccion-28').addClass("d-none")
+                        $('.seccion-29').addClass("d-none")
+                        $('.seccion-20').addClass("d-none")
+                        $('.seccion-26').removeClass("d-none")
+                        $('#radio_depended_146_0').change(() => {
+                            $('.seccion-28').removeClass("d-none")
+                            $('#radio_depended_137_0').change(() => {
+                                $('.seccion-29').removeClass("d-none")
+                            });
+                            $('#radio_depended_137_1').change(() => {
+                                $('.seccion-29').addClass("d-none")
+                            });
+                        });
+                        $('#radio_depended_146_1').change(() => {
+                            $('.seccion-28').removeClass("d-none")
+                            $('#radio_depended_137_0').change(() => {
+                                $('.seccion-29').removeClass("d-none")
+                            });
+                            $('#radio_depended_137_1').change(() => {
+                                $('.seccion-29').addClass("d-none")
+                            });
+                        });
+                    });
+                });
+                $('#radio_depended_127_1').change(() => {
+                    $('.seccion-28').removeClass("d-none")
+                    $('.seccion-17').addClass("d-none")
+                    $('.seccion-8').addClass("d-none")
+                    $('.seccion-9').addClass("d-none")
+                    $('.seccion-18').addClass("d-none")
+                    $('.seccion-20').addClass("d-none")
+                    $('.seccion-21').addClass("d-none")
+                    $('.seccion-22').addClass("d-none")
+                    $('.seccion-23').addClass("d-none")
+                    $('.seccion-24').addClass("d-none")
+                    $('.seccion-26').addClass("d-none")
+                    $('.seccion-29').addClass("d-none")
+                    $('#radio_depended_137_0').change(() => {
+                        $('.seccion-29').removeClass("d-none")
+                    });
+                    $('#radio_depended_137_1').change(() => {
+                        $('.seccion-29').addClass("d-none")
+                    });
+                });
             });
+            
+
+
         }
 
     }
