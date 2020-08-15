66eb408e22b09933f43a8e1a3e6cee1ceb313425
Herney Ruiz <herneyruiz36@gmail.com>
2020-08-15 07:17:01 -0500

    add new modelu observation

---
+++ database/factories/ObservationFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\observation;
+use Faker\Generator as Faker;
+
+$factory->define(observation::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
