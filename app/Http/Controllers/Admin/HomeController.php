25b3c319b51bef76f739d5afa9177ecd50e95575
Herney Ruiz <herneyruiz36@gmail.com>
2020-08-19 09:21:24 -0500

    new indicator test list

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -32,11 +32,17 @@ class HomeController extends Controller
     {
         $daySurveyCount = Survey::whereDate('created_at', Carbon::now()->format('Y-m-d'))->get()->count();
         $totalSurveyCount = Survey::all()->count();
-        $totalTestCount = Survey::where("header_id", 2)->count();
+        $totalTestCount = Survey::where("header_id", 6)->count();
 
         $totalcasesPositive = User::where("status", 1)->get()->count();
-        $totalcasesNegative = User::where("status", 2)->get()->count();
-        $totalcasesRecovered = User::where("status", 3)->get()->count();
+        $surveys = Survey::where("header_id", 6)->get();
+        $totalcasesNegative = 0;
+        foreach ($surveys as $survey) {
+            if ($survey->surveyed->where("status", 3)) {
+                $totalcasesNegative++;
+            }
+        }
+        $totalcasesRecovered = User::where("status", 2)->get()->count();
 
         return view('pages.admin.home', compact('daySurveyCount', 'totalSurveyCount', 'totalTestCount', 'totalcasesPositive', 'totalcasesNegative', 'totalcasesRecovered'));
     }
