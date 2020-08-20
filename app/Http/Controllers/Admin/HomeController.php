15cf7505a734f2e8682fe98197bd9d7c1fd11f09
Ricardo Erira <ricardoerira@gmail.com>
2020-08-20 00:55:30 -0500

    simplification of negative search for covid-19

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Answer;
+use App\Models\Observation;
 use App\Models\Survey;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 /**
  * HomeController
@@ -35,13 +37,10 @@ class HomeController extends Controller
         $totalTestCount = Survey::where("header_id", 6)->count();
 
         $totalcasesPositive = User::where("status", 1)->get()->count();
+        
         $surveys = Survey::where("header_id", 6)->get();
         $totalcasesNegative = 0;
-        foreach ($surveys as $survey) {
-            if ($survey->surveyed->where("status", 3)) {
-                $totalcasesNegative++;
-            }
-        }
+        $totalcasesNegative = Observation::where('observation', 'Caso negativo para covid-19')->count();
         $totalcasesRecovered = User::where("status", 2)->get()->count();
 
         return view('pages.admin.home', compact('daySurveyCount', 'totalSurveyCount', 'totalTestCount', 'totalcasesPositive', 'totalcasesNegative', 'totalcasesRecovered'));
